73b654f0d24d2ddfcd3d2f4c4db42628573629d8
Hendri Wahyu Perdana <hendriwp123@gmail.com>
2024-04-06 15:04:53 +0700

    fix: bugs package json for prod

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
 const init = async () => {
+  const NODE_ENV =
+    process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost';
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    host: NODE_ENV,
     routes: {
       cors: {
         origin: ['*'],
@@ -12,6 +14,7 @@ const init = async () => {
     },
   });
 
+  console.log(process.env.NODE_ENV);
   server.route(routes);
 
   await server.start();
