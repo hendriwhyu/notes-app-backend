424f360be3c6ab966430ca55d613226c26885dcc
Hendri Wahyu Perdana <hendriwp123@gmail.com>
2024-07-30 20:07:34 +0700

    user registration implemented

--- src/exceptions/NotFoundError.js
+++ src/exceptions/NotFoundError.js
@@ -1,4 +1,6 @@
-class NotFoundError extends Error {
+const ClientError = require('./ClientError');
+
+class NotFoundError extends ClientError {
   constructor(message) {
     super(message, 404);
     this.name = 'NotFoundError';
